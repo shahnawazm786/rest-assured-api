b555f56c8325d0a1348c49d23fa72fee94500ad6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-03-07 13:11:15 +0530

    rest assured

--- src/test/java/apis/jira/GivenExceptWhenThen.java
+++ src/test/java/apis/jira/GivenExceptWhenThen.java
@@ -1,6 +1,7 @@
 package apis.jira;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
@@ -20,9 +21,29 @@ public class GivenExceptWhenThen {
                 .then().assertThat().statusCode(404);
     }
     @Test
-    public  void givenExceptNotFound(){
+    public  void givenExceptNotFound1(){
         RestAssured.baseURI="https://reqres.in/";
-        given().expect().when().get("api/users/23")
-                .then().assertThat().statusCode(404);
+        String resp = given().expect().when().get("api/users/2")
+                .then().extract().response().asString();
+        System.out.println(resp);
+        JsonPath path=new JsonPath(resp);
+        System.out.println(path.getString("data.id"));
+        System.out.println(path.getString("data.email"));
+        System.out.println(path.getString("data.first_name"));
+        System.out.println(path.getString("data.last_name"));
+        System.out.println(path.getString("data.avatar"));
+    }
+    @Test
+    public  void getHeader(){
+        RestAssured.baseURI="https://reqres.in/";
+        String resp = given().expect().when().get("api/users/2")
+                .then().extract().response().asString();
+        System.out.println(resp);
+        JsonPath path=new JsonPath(resp);
+        System.out.println(path.getString("data.id"));
+        System.out.println(path.getString("data.email"));
+        System.out.println(path.getString("data.first_name"));
+        System.out.println(path.getString("data.last_name"));
+        System.out.println(path.getString("data.avatar"));
     }
 }
