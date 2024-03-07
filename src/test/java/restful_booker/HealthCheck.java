bf4b6a69eeacb70e809fe1271bfdb7613307efaa
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-03-07 14:44:55 +0530

    rest assured

--- src/test/java/restful_booker/HealthCheck.java
+++ src/test/java/restful_booker/HealthCheck.java
@@ -1,9 +1,13 @@
 package restful_booker;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
 
 public class HealthCheck {
 
@@ -15,6 +19,12 @@ public class HealthCheck {
     }
     @Test
     public void healthCheckPingMethod(){
+        String resp = given()
+                .when()
+                .get(IPathParam.PING_PATH_PARAM)
+                .then().extract().statusLine();
+        System.out.println(resp);
+        assertEquals("HTTP/1.1 201 Created",resp);
 
     }
 }
